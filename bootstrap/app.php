63614f3af72cd2b9ab9648dc2c57cfb20691506b
Abrar Ahmad <abrar.dev99@gmail.com>
2025-01-28 23:03:15 +0500

    Customize not found exception message

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,5 +16,11 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->renderable(function (NotFoundHttpException $e, $request) {
+            if ($request->is('api/decode/*')) {
+                return response()->json([
+                    'message' => 'Url not found.',
+                ], 404);
+            }
+        });
     })->create();
