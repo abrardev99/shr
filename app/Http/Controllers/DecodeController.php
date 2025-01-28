cbd2aa863eb6f96d649538cf353144ba02af58ac
Abrar Ahmad <abrar.dev99@gmail.com>
2025-01-28 23:29:14 +0500

    add postman collection

--- app/Http/Controllers/DecodeController.php
+++ app/Http/Controllers/DecodeController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\ShortUrl;
+use Illuminate\Http\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 class DecodeController
 {
-    public function __invoke(ShortUrl $shortUrl)
+    public function __invoke(ShortUrl $shortUrl): JsonResponse
     {
         return response()->json([
             'url' => $shortUrl->original_url,
